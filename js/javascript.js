7c14e0843335b2e190f776725cc18528ca01ede9
Gaia Iturralde <138412962+Gitrua07@users.noreply.github.com>
2025-05-30 16:57:47 -0700

    Fixed connectivity bug between button and user
    Button would not respond to click, but now
    it will output numbers based on the button
    the user has clicked on.

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,7 @@
+const btn = document.querySelector('.numbers');
+const screen = document.querySelector('.screen');
+let result = undefined;
+
 function add(num1, num2){
     return num1 + num2;
 }
@@ -17,20 +21,72 @@ function divide(num1, num2){
 function operator(num1, operator, num2){
     switch(operator){
         case '+':
-            add(num1, num2);
+            return add(num1, num2);
         case '-':
-            subtract(num1,num2);
+            return subtract(num1,num2);
         case '*':
-            multiply(num1, num2);
+            return multiply(num1, num2);
         case '/':
-            divide(num1,num2);
+            return divide(num1,num2);
         default:
-            ' ';
+            return ' ';
     }
     
 }
 
-let num1;
-let operator;
-let num2;
+let num1 = '';
+let operator1 = undefined;
+let num2 = '';
 
+btn.addEventListener('click', (event) =>{
+    const value = event.target.textContent.trim();
+    const num = Number(value);
+
+    screen.textContent += ` ${value}`;
+
+    if(value == 'C'){
+        num1 = '';
+        operator1 = undefined;
+        num2 = '';
+        screen.textContent = '';
+    }
+
+    if(value == '=' && num1 != undefined && operator1 != undefined &&
+        num2 != undefined
+    ){
+        result = operator(Number(num1), operator1, Number(num2));
+        screen.textContent = result;
+        operator1 = undefined;
+        num2 = '';
+        num1 = String(result);
+    }
+
+    if(operator1 == undefined && Number.isInteger(num)){
+        num1 += value;
+    }
+    
+    if(operator1 == undefined && num1 != undefined){
+        switch(value){
+            case '×':
+                operator1 = '*';
+                break;
+            case '÷':
+                operator1 = '/';
+                break;
+            case '+':
+                operator1 = '+';
+                break;
+            case '–':
+                operator1 = '-';
+                break;
+        }
+
+    }
+
+    if(num1!= '' && operator1 != undefined && Number.isInteger(num)){
+        num2 += value;
+    }
+    
+    console.log(`num1: ${num1} num2: ${num2}, operator: ${operator1}, result: ${result}`);
+
+})
