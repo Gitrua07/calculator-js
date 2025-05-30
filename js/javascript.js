1cf3d092af314fc9da75103240f28422c239d4a0
Gaia Iturralde <138412962+Gitrua07@users.noreply.github.com>
2025-05-30 15:58:39 -0700

    Added ui using index.html and style.css

--- js/javascript.js
+++ js/javascript.js
@@ -14,6 +14,23 @@ function divide(num1, num2){
     return num1/num2;
 }
 
+function operator(num1, operator, num2){
+    switch(operator){
+        case '+':
+            add(num1, num2);
+        case '-':
+            subtract(num1,num2);
+        case '*':
+            multiply(num1, num2);
+        case '/':
+            divide(num1,num2);
+        default:
+            ' ';
+    }
+    
+}
+
 let num1;
 let operator;
 let num2;
+
